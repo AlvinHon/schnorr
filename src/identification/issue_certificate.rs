1e0583933d73340da1b6b9729679bbcf03e463c7
AlvinHon <alvin.cpp@gmail.com>
2025-08-10 20:19:26 +0800

    Add method identity to struct IssueCertificate

--- src/identification/issue_certificate.rs
+++ src/identification/issue_certificate.rs
@@ -10,3 +10,10 @@ pub struct IssueCertificate<G: Group> {
     pub(in crate::identification) params: IssueParams<G>,
     pub(in crate::identification) s: Vec<u8>,
 }
+
+impl<G: Group> IssueCertificate<G> {
+    /// The identity specified in the certificate.
+    pub fn identity(&self) -> G::P {
+        self.params.i.clone()
+    }
+}
